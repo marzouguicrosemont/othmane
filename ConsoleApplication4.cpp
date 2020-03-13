13cd1087aec7edcaa747091bb04673ef88497612
Othmane Marzougui <6155414@crosemont.qc.ca>
2020-03-13 11:31:54 -0400

    Ajout d'une salutation

--- ConsoleApplication4.cpp
+++ ConsoleApplication4.cpp
@@ -1,35 +1,69 @@
-
 #include <iostream>
 using namespace std;
 
 int PGCD(int nombre1, int nombre2);
+
 int quantite_de_fractions(int nombre1, int nombre2);
 
-
+void simplifie(int x, int y);
 
 int main()
 {
-    char rep ;
+    int nombre11, nombre12, nombre_fraction;
+    int k = 0;
+   
 
-    do {
+   
+    
 
     
-    cout << "calculer le PGCD." << endl;
-    cout << "-----------------------------" << endl;
-
-    int nombre11, nombre12;
-    cout << "Enter deux nombres  differnets de zero svp:\t";
-    cin >> nombre11 >> nombre12;
-   
-    quantite_de_fractions(nombre11, nombre12);
-
-    cout << "-----------------------------" << endl;
-    cout << "voulez vous contenu" << endl;
-    cin >> rep;
-} while (rep != 'n' && rep != 'N');
 
 
-return 0;
+        cout << "calculer le PGCD." << endl;
+        cout << "-----------------------------" << endl;
+       
+
+       
+        do{
+            cout << "Enter nombre_fraction superieur a zero svp:\t";
+            cin >> nombre_fraction;
+        } while (nombre_fraction <= 0);
+        int h = nombre_fraction*2;
+        int t['h'];
+
+        for(int  i=0;i< nombre_fraction;i++)
+
+        {
+        
+        cout << "Entrez le Numerateur et Denominateur differents de zero ou un caractere pour quitter svp:\t";
+        cin >> nombre11 >> nombre12;
+        if (cin.fail()) {
+            cout << "by by" << endl;  goto ss;
+
+        } 
+        
+
+
+        quantite_de_fractions( nombre11,  nombre12);
+
+        cout << "-----------------------------" << endl;
+        
+        t[k] = nombre11;
+        t[k+1] = nombre12;
+       
+        simplifie(t[k], t[k+1]);
+        k=k+2;
+        cout << "-----------------------------" << endl;
+        
+        }
+        for(int j=0;j< nombre_fraction * 2;j++)
+        {
+            simplifie(t[j], t[j + 1]);
+            j=j+2;
+        }
+
+ss:;
+    return 0;
 }
 
 
@@ -46,26 +80,22 @@ return 0;
 
 int quantite_de_fractions(int nombre1, int nombre2)
 {
-    int N1, N2;
    
-    N1 = nombre1;
-    N2 = nombre2;
-    
 
-    
-        while (nombre1 == 0 || nombre2 == 0)
-        {
-            cout << "Enter deux nombres  differnets de zero svp:\t";
-            cin >> nombre1 >> nombre2;
-        }
-        cout << endl;
-        cout << "Le Plus Grand Commun Diviseur (PGCD) est:"<<PGCD(nombre1, nombre2) << endl;
-        
-        
-        
-   
-    return nombre1;
-    
+
+    while (nombre1 == 0 || nombre2 == 0)
+    {
+        cout << "Enter deux nombres  differnets de zero svp:\t";
+        cin >> nombre1 >> nombre2;
+    }
+    cout << endl;
+    cout << "Le Plus Grand Commun Diviseur (PGCD) est:" << PGCD(nombre1, nombre2) << endl;
+
+
+
+
+    return nombre1,nombre2;
+
 }
 
 
@@ -75,31 +105,36 @@ int quantite_de_fractions(int nombre1, int nombre2)
 
 int PGCD(int nombre1, int nombre2)
 {
+   
     int rest;
     if (nombre1 < 0) nombre1 = -nombre1;
     if (nombre2 < 0) nombre2 = -nombre2;
-
    
-   do {
+    do {
 
-       if(nombre1>nombre2)
-       {
-           rest = nombre1 - nombre2;
-           nombre1 = rest;
-
-
-       }
-       else if (nombre2 > nombre1)
-       {
-           rest = nombre2 - nombre1;
-           nombre2 = rest;
+        if (nombre1 > nombre2)
+        {
+            rest = nombre1 - nombre2;
+            nombre1 = rest;
 
 
-       }
+        }
+        else if (nombre2 > nombre1)
+        {
+            rest = nombre2 - nombre1;
+            nombre2 = rest;
 
-   } while (nombre1!= nombre2);
-   return nombre1;
+
+        }
+
+    } while (nombre1 != nombre2);
    
+  
+    return nombre1;
 }
+void simplifie(int x, int y)
+{
+    cout <<x << "/" << y << ">>>" << x / PGCD(x, y) << "/" << y / PGCD(x, y) << endl;
 
- 
+
+}
